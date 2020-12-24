6d4e0121ac0f06bb3410a8e2f8a64e0a8735c3e5
Tuany Fortunato do Carmo <tuany.fortunato1990@gmail.com>
2020-12-23 23:07:08 -0300

    Adicionando o knockout e o Sqlite

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,11 +1,55 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
 
 namespace MvcMovie.Controllers
 {
-    public class Movies : Controller
+    public class MoviesController : Controller
     {
-        public IActionResult Index(){
+        private readonly MvcMovieContext _context;
+
+        public MoviesController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(){
+            var movie = await _context.Movie.ToListAsync();
+            
+            if (!movie.Any())
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Edit(int? id){
+            if (id == null)
+            {
+                return NotFound();
+            }
             return View();
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
         }
     }
 }
